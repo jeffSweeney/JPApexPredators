92d6d4b1e91b6e26b0077ab6c49dcc0a83e6e341
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-05-25 13:46:15 -0400

    Model the JSON data

---
+++ JPApexPredators/JPApexPredators/Models/ApexPredator.swift
@@ -0,0 +1,33 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators
+//
+//  Created by Jeffrey Sweeney on 5/25/23.
+//
+
+import Foundation
+
+struct ApexPredator: Codable {
+    let id: Int
+    let name: String
+    let type: String
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, type, movies, link
+        case movieScenes = "movie_scenes"
+    }
+}
+
+struct MovieScene: Codable {
+    let id: Int
+    let movie: String
+    let sceneDescription: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, movie
+        case sceneDescription = "scene_description"
+    }
+}
